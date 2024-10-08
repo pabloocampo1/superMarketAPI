9ef4e3157c72d733579f9c6af8b4094a2a5ae37f
pablo ocampo <juanpabloocampoleon@gmail.com>
2024-10-08 18:43:51 -0500

    mi first commit

---
+++ src/test/java/com/market/superMarket/SuperMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.market.superMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
